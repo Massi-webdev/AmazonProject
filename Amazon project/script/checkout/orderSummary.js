3f34dd464afbb72afd0421c13e367006e74cefe8
Massi-webdev <ouiddir.massia@gmail.com>
2024-12-14 20:42:29 -0800

    Created and exported getProduct function to get the matching product

--- Amazon project/script/checkout/orderSummary.js
+++ Amazon project/script/checkout/orderSummary.js
@@ -4,6 +4,7 @@ import formatCurrency from "../utils/money.js";
 import { hello } from "https://unpkg.com/supersimpledev@1.0.1/hello.esm.js"
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js"
 import { deliveryOptions} from "../../data/deliveryOptions.js"
+import { countOrderTotal } from "./paymentSummary.js";
 
 
 /// Used MVC (Model -> View -> controller) loop method
@@ -13,7 +14,7 @@ import { deliveryOptions} from "../../data/deliveryOptions.js"
 
 
 
-updateCheckoutTotalItem();      //Update items numbers HTML (header + order summury items)
+
 renderOrderSummary();           //Create cart Items HTML
 countOrderTotal();              //total order price
 console.log(cart);
@@ -21,6 +22,9 @@ console.log(cart);
 
 /////////////////////////////////////////////// Create cart Items HTML ///////////////////////////////////////////////
 export function renderOrderSummary(){
+
+  updateCheckoutTotalItem();      //Update items numbers HTML (header + order summury items)
+
   let cartItemsHTML = '';           
 
   /// ---------------------Compare products with cart item to find the matching item
@@ -278,90 +282,20 @@ export function renderOrderSummary(){
     countOrderTotal() //-- recalculate total after updating car items quantity
   }
   //--------------------------------------------------------------------------------------------------------------------------
+
+
+
+  /////////////////////////////////////// Update checkout Header link ////////////////////////////////////////////////////////
+  function updateCheckoutTotalItem(){
+    document.querySelector('.js-checkout-link').innerHTML = `${updateCartQuntity()} Items`;
+    document.querySelector('.js-order-summary-total-items').innerHTML = `Items (${updateCartQuntity()}):`;
+  }
+
 }
 
 
 
 
-/////////////////////////////////////// Update checkout Header link ////////////////////////////////////////////////////////
-function updateCheckoutTotalItem(){
-  document.querySelector('.js-checkout-link').innerHTML = `${updateCartQuntity()} Items`;
-  document.querySelector('.js-order-summary-total-items').innerHTML = `Items (${updateCartQuntity()}):`;
-}
-//--------------------------------------------------------------------------------------------------------------------------
 
-
-
-
-/////////////////////////////////////// Count order Total //////////////////////////////////////////////////////////////////
-function countOrderTotal(){
-
-  let orderTotal = 0;
-  let ShippingPriceTotal = 0;
-
-  cart.forEach(cartItem=>{
-
-    const cartItemId = cartItem.productId;
-
-    ShippingPriceTotal+=cartItem.shippingPrice;
-    
-    products.forEach(product =>{
-      if(cartItemId===product.id){
-        orderTotal+=product.priceCents * cartItem.quantity;
-      }
-    });
-  });
-
-  const totalBeforeTax = orderTotal + ShippingPriceTotal;
-  const tax = totalBeforeTax * 0.1
-  const totalAfterTax = totalBeforeTax + tax;
-
-  document.querySelector('.js-payment-summary').innerHTML=
-  `
-    <div class="js-payment-info">
-          <div class="payment-summary-title"> Order Summary </div>
-    
-          <div class="payment-summary-row">
-            <div class="js-order-summary-total-items">Items (${updateCartQuntity()}):</div>
-            <div class="payment-summary-money"> $${formatCurrency(orderTotal)} </div>
-          </div>
-    
-          <div class="payment-summary-row">
-            <div>Shipping &amp; handling:</div>
-            <div class="payment-summary-money"> $${formatCurrency(ShippingPriceTotal)} </div>
-          </div>
-    
-          <div class="payment-summary-row subtotal-row js-subtotal-row">
-            <div class="total-before-tax">Total before tax:</div>
-            <div class="payment-summary-money payment-summary-money-tbt"> $${formatCurrency(totalBeforeTax)} </div>
-          </div>
-    
-          <div class="payment-summary-row">
-            <div>Estimated tax (10%):</div>
-            <div class="payment-summary-money"> $${formatCurrency(tax)} </div>
-          </div>
-    
-          <div class="payment-summary-row total-row">
-            <div>Order total:</div>
-            <div class="payment-summary-money"> $${formatCurrency(totalAfterTax)} </div>
-          </div>
-    </div>
-
-    <div class="paypal-toggle">
-          Use PayPal <input type="checkbox" class="js-paypal-toggle" false="">
-    </div>
-
-    <div class="js-payment-buttons-container false">
-
-          <div class="js-paypal-button-container paypal-button-container"></div>
-  
-          <button class="js-place-order-button place-order-button button-primary" d="">
-            Place your order
-          </button>
-    </div>
-
-  `
-}
-//-------------------------------------------------------------------------------------------------------------------------------
 
 
