d7c390ad93df177332df7e92e28ba40f1d9e9ecc
Massi-webdev <ouiddir.massia@gmail.com>
2024-12-18 20:27:43 -0800

    Recreate cart-oop for practice

--- Amazon project/data/cart-oop.js
+++ Amazon project/data/cart-oop.js
@@ -1,93 +1,104 @@
 function Cart(localStorageKey){
-  
+
   const cart = {
-  cartItems: undefined,
 
-  loadFromStorage() {          //--- change 'cart' to 'this' => otherwise if we change cart name, the code will stop working
-    this.cartItems = JSON.parse(localStorage.getItem(localStorageKey)) || [{
-      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-      quantity: 1,
-      deliveryOptionId: '1'
-    }];
-  },
+    cartItems:undefined,
 
-  /////////////////////////////// Function to save the cart items /////////////////////////////////////////
-  saveCartItem(){
-    localStorage.setItem(localStorageKey, JSON.stringify(this.cartItems))
-  },
-  
-
-  ///////////////////////////////////// AddtoCart /////////////////////////////////////////////////////////
- AddToCart(productId){
-    let matchingItem;
-    (this.cartItems).forEach(cartItem => {
-      
-      if (productId=== cartItem.productId){
-        matchingItem = cartItem;
-      } 
-    });
-
-    if (matchingItem){
-      matchingItem.quantity += 1;
-    } 
-    else {
-      (this.cartItems).push({
-        productId,
+    loadFromStorage() {
+      this.cartItems = JSON.parse(localStorage.getItem(localStorageKey)) || [{
+        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
         quantity: 1,
-        deliveryOptionId:'1',
-    });
-    };
-    this.saveCartItem()
-  },
-
-  
-  ////////////////////////// Function to count Items on cart and render the number /////////////////////////
-  updateCartQuntity(){
-    let totatCartItem = 0;
-    (this.cartItems).forEach(item => {
-      totatCartItem+=item.quantity;
-    });
-    
-    return totatCartItem
-  },
+        deliveryOptionId: '1'
+      }];
+    },
 
 
-  //////////////////////// Function remove item form car //////////////////////////////////////////////////
-  removeFromCart(productId){
-    const newCart = [];
-    (this.cartItems).forEach((cartItem)=>{
-      if (cartItem.productId!==productId){
-        newCart.push(cartItem);
-      }
-    })
-    this.cartItems = newCart;
-    this.saveCartItem();
-  },
-  
+    /////////////////////////////// Function to save the cart items /////////////////////////////////////////
+    saveCartItem(){
+      localStorage.setItem(localStorageKey, JSON.stringify(this.cartItems))
+    },
 
-  ////////////////////////// Function update Delivery Option///////////////////////////////////////////////
-  updateDeliveryOption(productId, deliveryOptionId){
-    let matchingItem;
 
-    (this.cartItems).forEach(cartItem => {
+    ////////////////////////// Function to count Items on cart and render the number /////////////////////////
+    updateCartQuntity(){
+      let totatCartItem = 0;
+      this.cartItems.forEach(item => {
+        totatCartItem += item.quantity;
+      });
+      return totatCartItem;
+    },
+
+
+    ///////////////////////////////////// AddtoCart /////////////////////////////////////////////////////////
+    AddToCart(productId){
+      let matchingItem;
+      this.cartItems.forEach(cartItem => {
+        // if car item = product we want to add => matching = item
         if (productId=== cartItem.productId){
-        matchingItem = cartItem;
-      } 
-    });
+          matchingItem = cartItem;
+        } 
+      });
 
-    matchingItem.deliveryOptionId = deliveryOptionId;
-    this.saveCartItem();
-  }
+      //  number of items we want to add
+      //let ItemsNumber = Number(document.querySelector(`.js-quntity-selector-${index}`).value)
+      
+      // if item already in the cart then just add +1
+      if (matchingItem){
+        matchingItem.quantity += 1;
+      } 
+      // else add it as new 
+      else {
+        this.cartItems.push({
+          productId,
+          quantity: 1,
+          deliveryOptionId:'1',
+      });
+      }
+      this.saveCartItem();
+    },
+
+    //////////////////////// Function remove item form car //////////////////////////////////////////////////
+    removeFromCart(productId){
+      const newCart = [];
+      this.cartItems.forEach((cartItem)=>{
+        if (cartItem.productId!==productId){
+          newCart.push(cartItem);
+        }
+      })
+      this.cartItems = newCart;
+      this.saveCartItem();
+    },
+
+    ////////////////////////// Function update Delivery Option///////////////////////////////////////////////
+    updateDeliveryOption(productId, deliveryOptionId){
+      let matchingItem;
+
+      this.cartItems.forEach(cartItem => {
+          if (productId=== cartItem.productId){
+          matchingItem = cartItem;
+        } 
+      });
+
+      matchingItem.deliveryOptionId = deliveryOptionId;
+      this.saveCartItem();
+    }
   };
 
   return cart;
 }
 
+
 const cart = Cart('cart-oop');
-const businessCart = Cart('cart-business');
+const businessCart = Cart('businessCart');
+
 
 cart.loadFromStorage();
 businessCart.loadFromStorage();
 
-console.log(cart);
-console.log(businessCart);
+
+
+console.log(cart.cartItems);
+console.log(businessCart.cartItems);
+
+
+
--- Amazon project/script/checkout.js
+++ Amazon project/script/checkout.js
@@ -2,7 +2,7 @@ import {renderOrderSummary} from "./checkout/orderSummary.js"
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { updateCheckoutHeader } from "./checkout/checkoutHeader.js";
 //import '../data/cart-class.js';   //Runs all the code without importing anything.
-
+import "../data/cart-oop.js"
 
 renderOrderSummary();
 renderPaymentSummary();
