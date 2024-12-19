4bbb42adbe1313b54e66b08d69901c063c7f183a
Massi-webdev <ouiddir.massia@gmail.com>
2024-12-18 21:07:27 -0800

    Recreated Cart Class to practice classes

--- Amazon project/data/cart-class.js
+++ Amazon project/data/cart-class.js
@@ -3,45 +3,49 @@ class Cart {
   
   // class property, it's like an object propertie / variable 
   //cartItems = undefined;  we can simply declare -- cartItems;
-  cartItems;         //public property : can be accessed anywhere
-  #localStorageKey;  //private property : used only inside the class  // not outside
+  cartItems;        //public property : can be accessed anywhere
+  localStorageKey;  //private property : used only inside the class  // not outside
 
-
+  
   // Constructors : help us run code when creating an object
   // has to be named constructor    +    should not return anything
   // Good place to put setup code
 
   constructor(localStorageKey){
-
-      // Since localStorageKey is undefined, We have have define local storage for each new object to generate object
-      this.#localStorageKey = localStorageKey;
-
-      //Load cart from local storage
-      this.#loadFromStorage();
-
+    this.localStorageKey = localStorageKey;
+    this.loadFromStorage(); 
   }
+ 
 
 
 
-  // class method1
-  #loadFromStorage() {             // make this method private too since we are using to set get item
-    this.cartItems = JSON.parse(localStorage.getItem(this.#localStorageKey)) || [{
+  // class method1 -----------------------------------------------------------
+  loadFromStorage() {
+    this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey)) || [{
       productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
       quantity: 1,
       deliveryOptionId: '1'
     }];
-  }
+  };
 
-  // class method2
+  // class method2 -----------------------------------------------------------
   saveCartItem(){
-    localStorage.setItem(this.#localStorageKey, JSON.stringify(this.cartItems))
-  }
+    localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems))
+  };
 
-  // class method3
+  // class method3 -----------------------------------------------------------
+  updateCartQuntity(){
+    let totatCartItem = 0;
+    this.cartItems.forEach(item => {
+      totatCartItem += item.quantity;
+    });
+    return totatCartItem;
+  };
+
+  // class method4 -----------------------------------------------------------
   AddToCart(productId){
     let matchingItem;
-    (this.cartItems).forEach(cartItem => {
-      
+    this.cartItems.forEach(cartItem => {
       if (productId=== cartItem.productId){
         matchingItem = cartItem;
       } 
@@ -51,42 +55,34 @@ class Cart {
       matchingItem.quantity += 1;
     } 
     else {
-      (this.cartItems).push({
+      this.cartItems.push({
         productId,
         quantity: 1,
         deliveryOptionId:'1',
     });
-    };
-    this.saveCartItem()
-  }
+    }
+    this.saveCartItem();
+  };
 
-  // class method4
-  updateCartQuntity(){
-    let totatCartItem = 0;
-    (this.cartItems).forEach(item => {
-      totatCartItem+=item.quantity;
-    });
-    
-    return totatCartItem
-  }
 
-  // class method5
+  // class method5 -----------------------------------------------------------
   removeFromCart(productId){
     const newCart = [];
-    (this.cartItems).forEach((cartItem)=>{
+    this.cartItems.forEach((cartItem)=>{
       if (cartItem.productId!==productId){
         newCart.push(cartItem);
       }
     })
     this.cartItems = newCart;
     this.saveCartItem();
-  }
+  };
 
-  // class method6
+
+  // class method6 -----------------------------------------------------------
   updateDeliveryOption(productId, deliveryOptionId){
     let matchingItem;
 
-    (this.cartItems).forEach(cartItem => {
+    this.cartItems.forEach(cartItem => {
         if (productId=== cartItem.productId){
         matchingItem = cartItem;
       } 
@@ -95,8 +91,8 @@ class Cart {
     matchingItem.deliveryOptionId = deliveryOptionId;
     this.saveCartItem();
   }
-
 };
+
 
 
 // Syntax look like functions   +  new to create a new object
@@ -108,9 +104,11 @@ console.log(businessCart);
 
 // To check if an object was generate by a class // a instance of a class
 console.log(businessCart instanceof Cart)   // =>  True
+console.log(!cart instanceof Cart)          // =>  False
 
-cart.#localStorageKey = 'test';  // when working on a team somebody accidentily might change that
+
 console.log(cart.localStorageKey);
+console.log(businessCart.localStorageKey);
 // to avoid we add #before a property to make it private and not accessible from outside the class
 // ERROR => Private field '#localStorageKey' must be declared in an enclosing class 
 
