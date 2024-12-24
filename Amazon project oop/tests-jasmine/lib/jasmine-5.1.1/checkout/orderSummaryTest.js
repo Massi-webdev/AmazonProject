9a7043b59d132817bf8d2492fba5797a56104e4e
Massi-webdev <ouiddir.massia@gmail.com>
2024-12-23 21:03:52 -0800

    load products in beforeAll hook

--- Amazon project oop/tests-jasmine/lib/jasmine-5.1.1/checkout/orderSummaryTest.js
+++ Amazon project oop/tests-jasmine/lib/jasmine-5.1.1/checkout/orderSummaryTest.js
@@ -1,18 +1,19 @@
 import { renderOrderSummary } from "../../../../script/checkout/orderSummary.js";
 import { AddToCart, cart, loadFromStorage } from "../../../../data/cart.js";
 import { updateCheckoutHeader } from "../../../../script/checkout/checkoutHeader.js";
-
+import { loadProduct } from "../../../../data/products.js";
 
 describe('test suite: Render Order Summary',()=>{
   
   const productID1 = "e43638ce-6aa0-4b85-b27f-e1d07eb678c6";
   const productID2 = "15b6fc6f-327a-4ec4-896f-486349e85a3d";
   
-  //beforeEach()  -> runs code before each test
-  //afterEach()  -> runs code after each test
-
-  //beforeall()  -> runs code before all test
-  //beforeEall()  -> runs code before all test
+  beforeAll((done) =>{
+    loadProduct(()=>{
+      done(); // allow to control when to go to the next step
+      //we can use done beforeEach and it
+    });
+  });
 
   beforeEach(()=>{ 
     document.querySelector('.js-test-container').innerHTML=
