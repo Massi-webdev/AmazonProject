baf4714e28fd2d4c4e6042077385ebef82ee8364
Massi-webdev <ouiddir.massia@gmail.com>
2024-12-23 21:14:23 -0800

    Tested products in jasmine using beforeAll hook and done()

--- Amazon project oop/tests-jasmine/lib/jasmine-5.1.1/checkout/orderSummaryTest.js
+++ Amazon project oop/tests-jasmine/lib/jasmine-5.1.1/checkout/orderSummaryTest.js
@@ -7,7 +7,11 @@ describe('test suite: Render Order Summary',()=>{
   
   const productID1 = "e43638ce-6aa0-4b85-b27f-e1d07eb678c6";
   const productID2 = "15b6fc6f-327a-4ec4-896f-486349e85a3d";
-  
+
+  function testLoadProduct(){
+    console.log('tested')
+  };
+
   beforeAll((done) =>{
     loadProduct(()=>{
       done(); // allow to control when to go to the next step
@@ -39,30 +43,30 @@ describe('test suite: Render Order Summary',()=>{
       ]);    //local storage support only strings
     });  
     loadFromStorage();
-
     renderOrderSummary();
+    
   });
 
 
   it('displays the cart', ()=>{
-    expect(document.querySelectorAll('.js-cart-item').length).toEqual(2);
+    expect(document.querySelectorAll('.js-cart-item').length).toEqual(3);
     expect(document.querySelector(`.item-quantity-${productID1}`).innerText).toContain('Quantiy: 1');
 
-    //expect(localStorage.getItem).toHaveBeenCalledWith('cart', `[]`);
-    
-    document.querySelector(`.js-delete-item-${productID1}`).click();
-
-    expect(document.querySelectorAll('.js-cart-item').length).toEqual(1);
+    expect(document.querySelectorAll('.js-cart-item').length).toEqual(3);
     expect(document.querySelector(`.js-cart-item${productID1}`)).not.toEqual(!null);
     expect(document.querySelector(`.js-cart-item${productID1}`)).toEqual(null);
-    expect(cart.length).toEqual(1);
-    expect(cart[0].productId).toEqual(productID2);
+    expect(cart.length).toEqual(2);
+    expect(cart[0].productId).toEqual(productID1);
 
     expect(document.querySelector('.item-price').innerText).toContain('$20.95');
-    
+  });
+
+
+  it('displays products from backEnd',()=>{
+    expect(loadProduct(testLoadProduct)).toEqual(console.log('tested'));
   });
   
   afterEach(()=>{
     document.querySelector('.js-test-container').innerHTML='' //delete all html to read clear results
-  })
+  });
 }); 
